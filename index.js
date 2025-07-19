f33daa47c5f893f946d902f9b65009687dc31b6f
Nandan Reddy S <nandan.bunny@gmail.com>
2025-07-19 12:55:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { App } = require("@slack/bolt");
 
 const app = new App({
 
-  token: "xoxb-9246010537840-9246024678096-4twLgQN8f3sjJSSHWvOv75dT",
-  signingSecret: "e712eca3bed82bec62d67e780a7ffba1",
+  token: "",
+  signingSecret: "",
 });
 
 (async () => {
